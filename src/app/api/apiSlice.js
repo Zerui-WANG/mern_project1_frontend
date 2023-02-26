af2a546efe5639d44a815f9bdccfc44f2ee4ab6c
Zerui-WANG <zerui_wang@yahoo.fr>
2023-02-26 17:10:57 +0100

    api url

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://technotes-u0hd.onrender.com",
+  baseUrl: "https://technotes-api-het5.onrender.com",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
